e6d080349db2a28f522ea0be4e5c77b7feca9f87
Adner C <adnerc@nycstudents.net>
2025-01-28 10:17:22 -0500

    added the advent starter code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Poker");
+
+        ArrayList<String> fileData = getFileData("src/InputFile");
+        // you now have a list of Strings from the file "InputFile"
+
+
     }
+
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+
 }
