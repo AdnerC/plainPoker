54d56a0ae3d2368b76be779d99aaff3fccc2970d
Adner C <adnerc@nycstudents.net>
2025-01-28 10:33:34 -0500

    Separated each card and bid amount

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/InputFile");
+        ArrayList<String> fileData = getFileData("src/TestInputFile");
         // you now have a list of Strings from the file "InputFile"
+        for (String fileDatum : fileData) {
+            int indexLine = fileDatum.indexOf("|");
+            int bidValue = Integer.parseInt(fileDatum.substring(indexLine+1));
+            String fileDatum2 = fileDatum.substring(0, indexLine);
+            String[] fileArray = fileDatum2.split(",");
+//            System.out.println(Arrays.toString(fileArray));
+//            System.out.println(bidValue);
+
+            for (String card : fileArray){
+                System.out.println(card);
+                int totalSimilarCount = 0;
+                for (int i = 1; i<fileArray.length-1;i++){
+                    if (card.equals(fileArray[i])){
+                        totalSimilarCount++;
+                    }
+
+                }
+            }
+        }
 
 
     }
