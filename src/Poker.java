21d40ccc320ae245378aad3f218359477f1aca65
HarleyY12 <hyang2496@gmail.com>
2025-01-29 11:33:24 -0500

    Added method to return card index from card type

--- src/Poker.java
+++ src/Poker.java
@@ -19,10 +19,18 @@ public class Poker {
     }
 
     public int countCards(){
-
         return 0;
     }
 
-
+    public int getCardIndex(String card){
+        //if a specific card type is found, its index is returned
+        String[] cardTypes = {"Ace","King","Queen","Jack","10","9","8",
+        "7","6","5","4","3","2","1"};
+        for (int i = 0; i < cardTypes.length; i++){
+            if(cardTypes[i].equals(card))
+                return i;
+        }
+        return -1;
+    }
 
 }
