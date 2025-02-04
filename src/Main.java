ece530bae38fbfcd5577a1d28ce0918e12d8a0f6
HarleyY12 <hyang2496@gmail.com>
2025-02-04 17:05:35 -0500

    Update Main.java
    Added comments as a summarizer for the functions of the main class

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Keeps track of different hand types
         int fiveOfAKind = 0;
         int fullHouse = 0;
         int fourOfAKind = 0;
@@ -13,8 +14,11 @@ public class Main {
         int twoPair = 0;
         int onePair = 0;
         int highCard = 0;
+        
+        //List to store all poker hands
         ArrayList<Poker> hands = new ArrayList<Poker>();
 
+        //List to store hands based on type
         ArrayList<Poker> fiveHands = new ArrayList<Poker>();
         ArrayList<Poker> fourHands = new ArrayList<Poker>();
         ArrayList<Poker> threeHands = new ArrayList<Poker>();
@@ -23,16 +27,16 @@ public class Main {
         ArrayList<Poker> oneHands = new ArrayList<Poker>();
         ArrayList<Poker> highHands = new ArrayList<Poker>();
 
-
-
+        //Reads input file and processes hands
         ArrayList<String> fileData = getFileData("src/TestInputFile");
         // you now have a list of Strings from the file "TestInputFile"
         for (String fileDatum : fileData) {
 
-
+            //Creates poker hand from file data
             Poker newPoker = new Poker(calcBidValue(fileDatum), getCardList(fileDatum));
             hands.add(newPoker);
 
+            //Identifies hand type and increments variables
             String hand = Poker.getHandType(getCardList(fileDatum));
             if(hand.equals("Five of a kind")){
                 fiveOfAKind++;
@@ -63,6 +67,8 @@ public class Main {
                 highHands.add(newPoker);
             }
         }
+
+        //Prints the number of each hand type
         System.out.println("Number of five of a kind hands: " + fiveOfAKind);
         System.out.println("Number of full house hands: " + fullHouse);
         System.out.println("Number of four of a kind hands: " + fourOfAKind);
@@ -73,6 +79,7 @@ public class Main {
         System.out.println();
         System.out.println("----------------------------------");
 
+        //Sort hands based on type
         Poker.sortHands(fiveHands);
         Poker.sortHands(fourHands);
         Poker.sortHands(threeHands);
@@ -81,8 +88,10 @@ public class Main {
         Poker.sortHands(oneHands);
         Poker.sortHands(highHands);
 
+        //Combine sorted list into one
         ArrayList<Poker> allHands =Poker.getHandSortedList(fiveHands,fourHands,fullHands,threeHands,twoHands,oneHands,highHands);
 
+        //Calculate to bid value
         int totalBidAmount=0;
         int rank =1;
 
@@ -94,11 +103,9 @@ public class Main {
         System.out.println("Total Bid Value: "+totalBidAmount);
         System.out.println("Total Bid Value With Jack Wild");
 
-
-
     }
 
-
+    //Verifies data file is present
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
@@ -115,13 +122,13 @@ public class Main {
             return fileData;
         }
     }
-
+    //Obtains bid value from each line
     public static int calcBidValue(String fileDatum){
         int indexLine = fileDatum.indexOf("|");
         int bidValue = Integer.parseInt(fileDatum.substring(indexLine+1).trim());
         return bidValue;
     }
-
+    //Returns the card list from each line
     public static String[] getCardList(String fileDatum){
         int indexLine = fileDatum.indexOf("|");
         String fileDatum1 = fileDatum.substring(0, indexLine);
